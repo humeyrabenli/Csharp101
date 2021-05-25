3e9fe593032139082305e68006ffa5c827bdad02
humeyrabenli <51988802+humeyrabenli@users.noreply.github.com>
2021-05-25 22:34:06 +0300

    Csharp101
    C# practices

---
+++ C#101/console-programming/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace console_programming
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("İsminizi giriniz:");
+            string name=Console.ReadLine();
+            Console.WriteLine("Soyadınızı giriniz:");
+            string surname=Console.ReadLine();
+
+            Console.WriteLine("Merhaba " + name + " " +surname);
+
+
+        }
+    }
+}
