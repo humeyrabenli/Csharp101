fa3be40d50695bf1d227b7d56d3b4a98ca3edb5f
humeyrabenli <51988802+humeyrabenli@users.noreply.github.com>
2021-06-01 19:44:38 +0300

    structs

---
+++ Csharp101/structs/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace structs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dikdortgen dikdortgen=new Dikdortgen();
+            //dikdortgen.Uzunkenar=4;
+            //dikdortgen.Kisakenar=3;
+            Console.WriteLine("Class alan hesabı  :{0}",dikdortgen.AlanHesapla());
+
+            Dikdortgen_struct dikdortgen_struct;
+            dikdortgen_struct.Kisakenar=3;
+            dikdortgen_struct.Uzunkenar=4;    
+            
+            Console.WriteLine("Class alan hesabı  :{0}",dikdortgen_struct.AlanHesapla());
+        }
+    }
+
+    class Dikdortgen
+    {
+        public int Kisakenar;
+        public int Uzunkenar;
+
+      /*   public Dikdortgen()
+        {
+            Kisakenar=3;
+            Uzunkenar=4;
+        } */
+        public long AlanHesapla() {
+            return this.Kisakenar*this.Uzunkenar;
+        }
+    }
+
+    struct Dikdortgen_struct
+    {
+        public int Kisakenar;
+        public int Uzunkenar;
+        public Dikdortgen_struct(int kisakenar, int uzunkenar)
+        {
+            
+            Kisakenar = kisakenar;
+            Uzunkenar = uzunkenar;
+        }
+        public long AlanHesapla() {
+            return this.Kisakenar*this.Uzunkenar;
+        }
+
+    }
+}
