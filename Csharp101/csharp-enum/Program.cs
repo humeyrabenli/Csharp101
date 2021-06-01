699e30c8cc0d34b331bfa4d5eb81a4aa90f8c65a
humeyrabenli <51988802+humeyrabenli@users.noreply.github.com>
2021-06-01 20:00:53 +0300

    enums

---
+++ Csharp101/csharp-enum/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace csharp_enum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Gunler.Pazartesi);
+            Console.WriteLine((int)Gunler.Cumartesi);
+            int sicaklik=32;
+            if (sicaklik<=(int)HavaDurumu.Normal)
+            {
+                Console.WriteLine("Dışarıya çıkmak için havanın biraz daha ısınmasını bekleyelim");
+            }
+            else if(sicaklik>=(int)HavaDurumu.Sicak) {
+                Console.WriteLine("Dışarıya çıkmak için çok sıcak bir gün");
+            }
+            else if(sicaklik>=(int)HavaDurumu.Normal && sicaklik<(int)HavaDurumu.CokSicak) {
+                Console.WriteLine("Hadi dışarıya çıkalım");
+            }
+        }
+    }
+
+    enum Gunler
+    {
+        Pazartesi=1,
+        Salı,
+        Çarşamba,
+        Perşembe,
+        Cuma=23,
+        Cumartesi,
+        Pazar
+    }
+
+    enum HavaDurumu
+    {
+        Soguk=5,
+        Normal=20,
+        Sicak=25,
+        CokSicak=30
+
+    }
+}
---
+++ Csharp101/enum/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace enum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
